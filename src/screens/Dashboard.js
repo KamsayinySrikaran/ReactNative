ff6f84521ba7a3e2c47fa88953997bdac5f30974
kamsayiny <kamshasris01@gmail.com>
2022-03-16 15:26:34 +0530

    ReactNative Mobile login

---
+++ src/screens/Dashboard.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import Background from '../components/Background'
+import Header from '../components/Header'
+
+export default function Dashboard({ navigation }) {
+  return (
+    <Background>
+      <Header>Let’s start</Header>
+    </Background>
+  )
+}
